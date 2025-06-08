1f77b77205e2dda32b041ada4ff5e5b32fd32970
tonyrufino <143525137+tonyrufino@users.noreply.github.com>
2025-06-08 19:24:57 -0300

    Add files via upload

--- Seminario Antonio Rufino.sql
+++ Seminario Antonio Rufino.sql
@@ -1,69 +1,59 @@
-create database TaskManager;
-use TaskManager;
+CREATE DATABASE `taskmanager` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 
-CREATE TABLE GRUPO (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL
-);
+CREATE TABLE `grupo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE USUARIO (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    rol VARCHAR(20) NOT NULL,
-    grupo_id INT,
-    FOREIGN KEY (grupo_id) REFERENCES GRUPO(id)
-);
+CREATE TABLE `proyecto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `vencimiento` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE PROYECTO (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    descripcion TEXT,
-    vencimiento DATE NOT NULL,
-    completo BOOLEAN
-);
+CREATE TABLE `tarea` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` text NOT NULL,
+  `prioridad` varchar(20) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL,
+  `vencimiento` date NOT NULL,
+  `idProyecto` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tarea_ibfk_1` (`idProyecto`),
+  CONSTRAINT `tarea_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyecto` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE TAREA (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    descripcion TEXT NOT NULL,
-    prioridad VARCHAR(20),
-    completa BOOLEAN,
-    vencimiento DATE NOT NULL,
-    proyecto_id INT NOT NULL,
-    FOREIGN KEY (proyecto_id) REFERENCES PROYECTO(id)
-);
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `grupo_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `grupo_id` (`grupo_id`),
+  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`grupo_id`) REFERENCES `grupo` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE USUARIO_TAREA (
-    usuario_id INT NOT NULL,
-    tarea_id INT NOT NULL,
-    PRIMARY KEY (usuario_id, tarea_id),
-    FOREIGN KEY (usuario_id) REFERENCES USUARIO(id),
-    FOREIGN KEY (tarea_id) REFERENCES TAREA(id)
-);
+CREATE TABLE `usuario_grupo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) NOT NULL,
+  `idGrupo` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idGrupo` (`idGrupo`),
+  CONSTRAINT `usuario_grupo_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `usuario_grupo_ibfk_2` FOREIGN KEY (`idGrupo`) REFERENCES `grupo` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- poblar tablas
-INSERT INTO `taskmanager`.`grupo` (`nombre`) VALUES ('RRHH');
-
-INSERT INTO `taskmanager`.`proyecto` (`nombre`, `descripcion`, `vencimiento`, `completo`) VALUES ('Generico', 'Generar un proyecto generico para la correción del TP', '2025-10-10', '0');
-
-INSERT INTO `taskmanager`.`tarea` (`descripcion`, `prioridad`, `completa`, `vencimiento`, `proyecto_id`) VALUES ('Testear Funcionalidad de la base de datos', 'Alta', '0', '2025-10-10', '1');
-
-INSERT INTO `taskmanager`.`usuario` (`nombre`, `rol`, `grupo_id`) VALUES ('CarlosRamirez', 'Usuario', '1');
-
-INSERT INTO `taskmanager`.`usuario_tarea` (`usuario_id`, `tarea_id`) VALUES ('1', '1');
-
-
--- consulta 1
-SELECT t.descripcion, g.nombre
-FROM TAREA t
-JOIN GRUPO g ON t.grupo_id = g.id
-WHERE g.nombre = 'RRHH';
-
--- consulta 2
-SELECT t.descripcion, t.estado, t.vencimiento
-FROM TAREA t
-JOIN USUARIO_TAREA ut ON t.id = ut.tarea_id
-JOIN USUARIO u ON ut.usuario_id = u.id
-WHERE u.nombre = 'CarlosRamirez';
-
-
-
+CREATE TABLE `usuario_tarea` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) NOT NULL,
+  `idTarea` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idTarea` (`idTarea`),
+  CONSTRAINT `usuario_tarea_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `usuario_tarea_ibfk_2` FOREIGN KEY (`idTarea`) REFERENCES `tarea` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
